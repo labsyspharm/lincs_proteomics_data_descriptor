b7738e945e070a54e50dde5b5c49f57d2d96561b
smkartik <sub.kartik@gmail.com>
2020-02-10 12:23:13 +0530

    update input data and R code for figure 3

--- src/Figure3.R
+++ src/Figure3.R
@@ -1,22 +1,46 @@
 library(UpSetR)
 library(corrplot)
-library(ggplotify)
 
 # -- Figure 3a ---------
-dfs <- read.csv(file='../data/upsetplot_input.csv')
+dfs <- read.csv(file='../data/upset_plot_input_8batches.csv')
 
-f3a <- upset(dfs, sets=c('batch1', 'batch2', 'batch3', 'batch4', 'batch5'), 
+f3a <- upset(dfs, sets=c('batch1', 'batch2', 'batch3', 'batch4', 
+                         'batch5', 'batch6', 'batch7', 'batch8'), sets.bar.color = "#56B4E9",
           mainbar.y.label = "proteins identified in common", sets.x.label = "batch size", 
-          text.scale = c(1.3, 1.3, 1, 1, 2, 0.75),
+          text.scale = c(2, 1.5, 2, 1.5, 1.5, 1.25),
           order.by="freq", keep.order=TRUE)
 
-# Figure 3b ------
-data <- read.csv('../data/non_malignants_samples.csv')
+pdf(file = 'upset_plot_allbatches.pdf', width=12, height=7)
+f3a
+dev.off()
+
+ # Fig 3B
+data <- read.csv('../data/input_qc_fig3b.csv')
+
+s1 <- ggbarplot(data, x='batch', y='avg_intensities',
+                 xlab='', ylab='Mean protein intensities', width=0.4, 
+                 title="") +
+  theme(axis.title.y = element_text( size = 12, face = "bold" ))
+
+s2 <- ggbarplot(data, x='batch', y='avg_num_peptides',
+                xlab='', ylab='Mean peptides per protein', width=0.4, 
+                title="") +
+  theme(axis.title.y = element_text( size = 12, face = "bold" ))
+
+s12 <- ggarrange(s1, s2, nrow=2)
+ggsave('Figure3B.pdf', dpi=300)
+
+# --- Figure 3C --------
+data <- read.csv('../data/unscaled_non_malignants_samples.csv')
+
 x <- cor(data)
-colnames(x) = c("MCF10A rep1", "MCF10A rep2", 'MCF10A rep3', 'hME1')
-rownames(x) = c("MCF10A rep1", "MCF10A rep2", 'MCF10A rep3', 'hME1')
-#f3b <- ggcorrplot(x, method = "circle", type='lower', lab=TRUE)
+colnames(x) = c("MCF10A rep1", "MCF10A rep2", 'MCF10A rep3', 
+                'MCF10A rep4', 'MCF10A rep5', 
+                'hME1')
+rownames(x) = c("MCF10A rep1", "MCF10A rep2", 'MCF10A rep3', 
+                'MCF10A rep4', 'MCF10A rep5', 
+                'hME1')
 f3b <- corrplot.mixed(x, lower.col = "black", number.cex = .7, tl.cex=0.8)
+ggsave('Figure3C.pdf', dpi=300)
 
 
-#f3ab <- ggarrange(f3a, f3b, nrow=1, ncol=2, widths=c(0.7, 0.3))
